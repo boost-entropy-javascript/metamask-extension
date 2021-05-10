d600878fb4c1d8665a18d10cd9d07b0311ed354e
Etienne Dusseault <etienne.dusseault@gmail.com>
2021-05-10 12:31:10 +0800

    whip

---
+++ ui/pages/confirm-approve/confirm-approve.stories.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import { select, text } from '@storybook/addon-knobs';
+import ConfirmApprove from './index'
+
+export default {
+  title: 'Confirm - Approve',
+};
+
+export const Approve = ({
+
+}) => (
+  <ConfirmApprove
+  />
+);
